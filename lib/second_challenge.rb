f79da34c6314002252943dc6fe05c45f6f9f09dd
Beth Ruff <beth.ruff2@gmail.com>
2020-02-02 17:30:41 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,12 +5,15 @@ def second_challenge
       :matriarch => {},
       :hero => {},
       :hero_friends => []
+    },
     :capulet => {
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
-      }
+    }
+  }
+  
 
   
 end
